0772599921e9e70d83b197c26f15760aba00b981
sechs <philippe.buteau@skema.edu>
2021-09-22 18:38:43 +0200

    commented counter component

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
 export default function Counter() {
+  // State init for the counter
   const [counter, setCounter] = useState(0);
 
+  // Counter func where we pass the argument of "incr" or "decr" to determine action
   const counterFunc = (action) => {
     if (action === "increment") {
       setCounter(counter + 1);
